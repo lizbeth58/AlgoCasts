a03c11a026cef950febeaaf4bc01ded828e60cf1
lizbeth58 <ortiz.beth58@gmail.com>
2019-12-25 02:51:10 -0800

    clocked out

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let maxChar = 0;
+  let map = {};
+
+  for (char of str) {
+    if (map[char].notExist) {
+      map[char] = 1;
+    } else {
+      map[char] += map[char]++;
+    }
+  }
+
+  for (key of map) {
+    maxChar = map[key];
+    if (map[key] > maxChar) {
+      maxChar = map[key];
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
