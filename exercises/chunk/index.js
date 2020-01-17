285e984a5a64f7ae796d94e9c3ed63a96b28d32b
lizbeth58 <ortiz.beth58@gmail.com>
2020-01-17 12:13:41 -0800

    solved chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,21 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // Create new array
+  const newArr = [];
+
+  //calcualte number of iterations by dividing by chunk size
+  //take the cieling incase of odd number
+  const length = Math.ceil(array.length / size);
+
+  for (let i = 0; i < length; i++) {
+    //splice from the beginning to chunk size and push to new array
+    //splice was used because it modifies the original array.
+    newArr.push(array.splice(0, size));
+  }
+
+  return newArr;
+}
 
 module.exports = chunk;
