906b0aa22cb3fae6940acee750a190a418494a4d
lizbeth58 <ortiz.beth58@gmail.com>
2020-01-17 13:03:07 -0800

    solved chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -12,15 +12,38 @@ function chunk(array, size) {
   // Create new array
   const newArr = [];
 
+  //Solution 1, Using SPLICE built in functions
+
   //calcualte number of iterations by dividing by chunk size
   //take the cieling incase of odd number
   const length = Math.ceil(array.length / size);
 
   for (let i = 0; i < length; i++) {
-    //splice from the beginning to chunk size and push to new array
-    //splice was used because it modifies the original array.
+    //splice from the beginning to chunk size and push to new array. splice was used because it modifies the original array.
     newArr.push(array.splice(0, size));
   }
+
+  //Solution 2, using SLICE bult in function
+
+  /*let index = 0;
+
+  while (index < array.length) {
+    newArr.push(array.slice(index, index + size));
+    index += size;
+  }*/
+
+  /*
+  //Solution 3
+  
+  for (let element of array) {
+    const last = newArr[newArr.length - 1];
+
+    if (!last || last.length === size) {
+      newArr.push([element]);
+    } else {
+      last.push(element);
+    }
+  }*/
 
   return newArr;
 }
