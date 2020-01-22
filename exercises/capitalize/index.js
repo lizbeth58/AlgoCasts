462aeeea2843a7605a992c2289ac0f4f6e91e7e4
lizbeth58 <ortiz.beth58@gmail.com>
2020-01-21 20:57:13 -0800

    capitalized solution 1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,18 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // split array at spaces
+  const arrayOfWords = str.split(' ');
+
+  //iterate through all words
+  //get the first character of every word and make it uppercase
+  //slice off the rest of the word and concatenate the uppercase character and the rest of the word
+  //return a new array with all first characters of every word in array uppercase
+  //join array into one string with a space between each word
+  return arrayOfWords
+    .map(word => (word = word[0].toUpperCase() + word.slice(1)))
+    .join(' ');
+}
 
 module.exports = capitalize;
